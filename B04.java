0a98fe84191d5c6056c071bbc587a09683f346f1
ghfjdvk <ghfjdvk@naver.com>
2016-10-07 10:57:22 +0900

    Update B04.java

--- B04.java
+++ B04.java
@@ -8,13 +8,13 @@ public class B04 {
 		System.out.print("연봉을 월단위로 입력하세요 ");
 		int money = scanner.nextInt();
 		if(money < 10000000)
-			tax = money * (9.5/100);
+			tax = money * 9.5/100;
 		else if(money>=10000000 && money<40000000)
-			tax = money * (19/100);
+			tax = money * 19/100;
 		else if(money >= 40000000 && money < 80000000)
-			tax = money * (28/100);
+			tax = money * 28/100;
 		else if(money >= 80000000)
-			tax = money * (37/100);
+			tax = money * 37/100;
 		System.out.println("연봉 금액에 대한 소득세는 "+tax+"입니다");
 		
 		
