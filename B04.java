b0695fe9ee368e18d7cf55af50bc5fbb56294e60
ghfjdvk <ghfjdvk@naver.com>
2016-10-07 10:37:38 +0900

    Update B04.java
    B04 수정

--- B04.java
+++ B04.java
@@ -1,1 +1,23 @@
-12345
+import java.util.*;
+public class B04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		double tax;
+		System.out.print("연봉을 웓단위로 입력하세요 ");
+		int money = scanner.nextInt();
+		if(money < 10000000)
+			tax = money * (9.5/100);
+		else if(money>=10000000 && money<40000000)
+			tax = money * (19/100);
+		else if(money >= 40000000 && money < 80000000)
+			tax = money * (28/100);
+		else if(money >= 80000000)
+			tax = money * (37/100);
+		System.out.println("연봉 금액에 대한 소득세는 "+tax+"입니다");
+		
+		
+	}
+
+}
